a137b169b22c9045eb82b654d8895c6fa3a98020
gemeoslemes <victorlemes0776@gmail.com>
2024-05-27 12:57:05 -0300

    Implementando Exceptions customizadas e os VO de Credenciais.

---
+++ src/main/java/br/com/erudio/execeptions/InvalidJwtAuthenticationException.java
@@ -0,0 +1,18 @@
+package br.com.erudio.execeptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serializable;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidJwtAuthenticationException extends AuthenticationException implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidJwtAuthenticationException(String msg) {
+        super(msg);
+    }
+
+}
--- src/main/java/br/com/erudio/execeptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/erudio/execeptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.erudio.execeptions.handler;
 
 import br.com.erudio.execeptions.ExceptionResponse;
+import br.com.erudio.execeptions.InvalidJwtAuthenticationException;
 import br.com.erudio.execeptions.RequiredObjectIsNotNullException;
 import br.com.erudio.execeptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -42,6 +43,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
     }
 
 }
