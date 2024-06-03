d26de748e5c397404774b06d6f2c2a0da0a38881
gemeoslemes <victorlemes0776@gmail.com>
2024-06-03 13:46:12 -0300

    Fixando os testes quebrados.

--- src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerJsonTest.java
+++ src/test/java/br/com/erudio/integrationtests/controllers/withjson/PersonControllerJsonTest.java
@@ -2,7 +2,9 @@ package br.com.erudio.integrationtests.controllers.withjson;
 
 import br.com.erudio.configs.TestConfigs;
 import br.com.erudio.integrationtests.AbstraticIntegrationTest;
+import br.com.erudio.integrationtests.vo.AccountCredentialsVO;
 import br.com.erudio.integrationtests.vo.PersonVO;
+import br.com.erudio.integrationtests.vo.TokenVO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import io.restassured.builder.RequestSpecBuilder;
@@ -33,25 +35,46 @@ public class PersonControllerJsonTest extends AbstraticIntegrationTest {
     }
 
     @Test
+    @Order(0)
+    public void testAuthorization() {
+        AccountCredentialsVO user = new AccountCredentialsVO("Victor", "12345678");
+
+        var accessToken = given()
+                .basePath("/auth/signin")
+                    .port(TestConfigs.SERVER_PORT)
+                    .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(user)
+                    .when()
+                .post()
+                .then()
+                        .statusCode(200)
+                            .extract()
+                            .body()
+                                .as(TokenVO.class)
+                            .getAccessToken();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+    }
+
+    @Test
     @Order(1)
     public void testCreate() throws JsonProcessingException {
         mockPerson();
 
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
-                .setBasePath("/api/person/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-                .build();
-
         var content = given()
                 .spec(specification)
                 .contentType(TestConfigs.CONTENT_TYPE_JSON)
-                .body(person)
-                .when()
-                .post()
-            .then()
+                    .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
+                    .body(person)
+                    .when()
+                    .post()
+                .then()
                     .statusCode(201)
                 .extract()
                     .body()
@@ -77,16 +100,11 @@ public class PersonControllerJsonTest extends AbstraticIntegrationTest {
     @Order(2)
     public void testCreateWithWrongOrigin() {
         mockPerson();
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_SEMERU)
-                .setBasePath("/api/person/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-                .build();
 
-        var content = given().spec(specification)
-                        .contentType(TestConfigs.CONTENT_TYPE_JSON)
+        var content = given()
+                .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_SEMERU)
                         .body(person)
                     .when()
                         .post()
@@ -105,17 +123,11 @@ public class PersonControllerJsonTest extends AbstraticIntegrationTest {
     @Order(3)
     public void testFindById() throws JsonProcessingException {
         mockPerson();
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
-                .setBasePath("/api/person/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-                .build();
 
         var content = given()
                 .spec(specification)
                 .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
                 .pathParam("id",person.getId())
                 .when()
                 .get("{id}")
@@ -147,16 +159,11 @@ public class PersonControllerJsonTest extends AbstraticIntegrationTest {
     @Order(4)
     public void findByIdWithWrongOrigin() {
         mockPerson();
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_SEMERU)
-                .setBasePath("/api/person/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .build();
 
-        var content = given().spec(specification)
+        var content = given()
+                .spec(specification)
                 .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_SEMERU)
                     .body(person)
                 .pathParam("id", person.getId())
                     .when()
